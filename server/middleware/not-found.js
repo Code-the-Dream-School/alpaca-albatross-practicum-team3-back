cb6f757717615f08a958f7ef56d16d1ccbad65c5
Chung Kao <chungckao@gmail.com>
2022-11-08 02:18:51 +0800

    Revert "Reorganized back-end code into a server directory sub to the root to co-occupy the repo with front end"

--- server/middleware/not-found.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-
-const notFound = (req, res) =>
-  res.status(404).sendFile(path.join(__dirname, "..", "public", "404.html"));
-
-module.exports = notFound;
--- src/components/form/LogOutButton.js
+++
@@ -1,9 +0,0 @@
-const LogOutButton = ({onClicked, children}) => (
-  <>
-    <button type='button' onClick={(e) => onClicked()}>
-      {children}
-    </button>
-  </>
-);
-
-export default LogOutButton;
