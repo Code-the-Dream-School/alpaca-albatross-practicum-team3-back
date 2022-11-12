306513b74c5d35ddc6331711f223099b14710f1b
Anna Liskovets <anna.liskovets@Annas-MacBook-Pro.local>
2022-11-12 12:29:29 -0500

    Git todo list items from backend API instead of local storage

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,35 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import AddTodoLabel from './AddTodoLabel';
 
-
-// This function assembles form with input field--sb 
+// This function assembles form with input field--sb
 
 const AddTodoForm = ({ addTodo }) => {
-    const [todoTitle, setTodoTitle] = useState("")
+  const [todoTitle, setTodoTitle] = useState('');
 
-    const handleTitleChange = (e) => {
-        const newTodoTitle = e.target.value;
-        setTodoTitle(newTodoTitle);
-    };
+  const handleTitleChange = (e) => {
+    const newTodoTitle = e.target.value;
+    setTodoTitle(newTodoTitle);
+  };
 
-    const handleAddTodo = (e) => {
-        e.preventDefault();
-        addTodo({ title: todoTitle, id: Date.now() });
-        setTodoTitle("");
-    };
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    addTodo({ id: Date.now(), title: todoTitle }); // Id here must be removed because DB has its own id
+    setTodoTitle('');
+  };
 
-    return (
-        <form onSubmit={handleAddTodo}>
-            <AddTodoLabel
-                todoTitle={todoTitle}
-                handleTitleChange={handleTitleChange}>
-                Title
-            </AddTodoLabel>
-            <button type="submit">Add</button>
-        </form>
-    );
+  return (
+    <form onSubmit={handleAddTodo}>
+      <AddTodoLabel todoTitle={todoTitle} handleTitleChange={handleTitleChange}>
+        Title
+      </AddTodoLabel>
+      <button type='submit'>Add</button>
+    </form>
+  );
 };
 
 export default AddTodoForm;
-
-
--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React, {useEffect, useState} from "react";
-import TodoList from "./TodoList";
-import {Routes, Route} from "react-router-dom";
-import FavoritesPage from "./FavePage/FavoritesPage";
-import Header from "./Header";
+import React from 'react';
+import TodoList from './TodoList';
+import { Routes, Route } from 'react-router-dom';
+import FavoritesPage from './FavePage/FavoritesPage';
+import Header from './Header';
 
 // Function to preserve list upon refresh. Works with local storage.--sb
 
-const useSemiPersistentState = () => {
+/*const useSemiPersistentState = () => {
   const [todoList, setTodoList] = useState(
-    JSON.parse(localStorage.getItem("savedTodoList")) || []
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
   );
 
   useEffect(() => {
-    localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]);
 
   return [todoList, setTodoList];
-};
+};*/
 
 function App() {
   return (
@@ -24,18 +24,10 @@ function App() {
       <Header />
       <Routes>
         {/* home */}
-        <Route
-          path='/'
-          element={
-            <TodoList useSemiPersistentState={useSemiPersistentState} />
-          }></Route>
+        <Route path='/' element={<TodoList />}></Route>
 
         {/* FavePage */}
-        <Route
-          path='/Favorites'
-          element={
-            <FavoritesPage useSemiPersistentState={useSemiPersistentState} />
-          }></Route>
+        <Route path='/Favorites' element={<FavoritesPage />}></Route>
       </Routes>
     </>
   );
