dad3307baaeeedb4b990fcd94648ea2e43bff786
Sherose Badruddin <sheroseb@gmail.com>
2022-12-17 17:00:49 -0500

    set taskbar style for desktop

--- frontend/src/pages/Home/NavbarElements.js
+++ frontend/src/pages/Home/NavbarElements.js
@@ -7,82 +7,66 @@ export const Nav = styled.nav`
   align-items: flex-end;
   flex-direction: row;
   justify-content:center;
-
-  // padding: 3px calc((100vw - 1000px) / 2);
   z-index: -1;
   border: 1px solid rgba(255, 255, 255, 0.25);
   border-radius: 10px;
   background-color: rgba(255, 255, 255, 0.45);
   box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(5px);
+
+  @media screen and (max-width: 768px) {
+  height: 50px;
+  }
 `;
 
 
 export const NavLink = styled(Link)`
-  // color: #fff;
-  // margin-bottom: -25px;
   font-size: 24px;
-  
-  // display: flex;
-  // align-items: center;
-  // text-decoration: none;
-  // padding: 0 1rem;
-  // height: 60%;
-  // cursor: pointer;
-  // border: solid black 1px;
-  // border-radius: 5px;
-
   border: 1px solid rgba(255, 255, 255, 0.25);
   border-radius: 5px;
   box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(1px);
+  width:25%;
 
   &:hover {
-    // color: #8799f3;
-    border: #fffff0 solid 3px;
+    box-shadow:0 0 10px 3px #fffff0;
+  }
+
+  @media screen and (max-width: 768px) {
+    min-width:fit-content;
   }
 `;
 
 
-// send to footer in @media
+// set tabs to footer in @media
 export const NavMenu = styled.div`
   display: flex;
-  // align-items: center;
-  margin-right: -24px;
+  width:40%;
 
   @media screen and (max-width: 768px) {
-    display: none;
+    // display: none;
   }
 `;
 
 export const NavBtn = styled.nav`
-  display: flex;
-  // align-items: center;
-  // margin-right: 24px;
   float: right;
-  margin-top: -50px;
-
-  @media screen and (max-width: 768px) {
-    display: none;
-  }
+  margin-top: -55px;
 `;
 
 
 export const NavBtnLink = styled(Link)`
-  border-radius: 4px;
-  font-size: 15px;
-  // background: #c8bbf4;
-  padding: 10px 15px;
-  color: #222831;
-  border: solid black 1px;
-  outline: none;
-  cursor: pointer;
-
-  text-decoration: none;
+  font-size: 24px;
+  margin-right: 10px;
+  margin-bottom: 5px;
+  border: 1px solid rgba(255, 255, 255, .25);
+  border-radius: 5px;
+  background-color: rgba(255, 255, 255, 0.45);
+  box-shadow: 0 0 10px 5px rgba(0, 0, 0, 0.25);
+  backdrop-filter: blur(5px);
+  padding:5px;
   z-index: 1;
 
   &:hover {
-    border: solid 4px;
-    border-color: #bcb89a;
+    box-shadow:0 0 10px 5px #fffff0;
   }
 `;
